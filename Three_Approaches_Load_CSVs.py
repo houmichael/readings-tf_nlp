897289d53e10763fe3549350951b7a777e05e376
houmichael <michael.hou@bronto1.com>
2023-12-10 09:15:45 -0500

    Change File Name

--- Three_Approaches_Load_CSVs.py
+++ Three_Approaches_Load_CSVs.py
@@ -1,11 +1,16 @@
+# Define the path to the CSV file
+emails_file_path = 'c:\\bronto1\\data\\emails.csv'
+
+# Loading the emails -> Approach 1.-> Read in the whole csv file as a string
+with open(emails_file_path, 'r') as emails_file:
+    emails_str = emails_file.read() 
+
+
 # Loading the emails -> Approach 2.->With PySpark
 from pyspark.sql import SparkSession
 
 # Create a Spark session
 spark = SparkSession.builder.appName("ReadCSV").getOrCreate()
-
-# Define the path to the CSV file
-emails_file_path = 'c:\\bronto1\\data\\emails.csv'
 
 # Read the CSV file with header, handling multiline and escaping special characters
 emails_spark_df = spark.read.format("csv"). \
@@ -16,3 +21,4 @@ emails_spark_df = spark.read.format("csv"). \
 # Loading the emails -> Approach 3.->With Pandas
 import pandas as pd
 emails_pandas_df = pd.read_csv(emails_file_path)
+
